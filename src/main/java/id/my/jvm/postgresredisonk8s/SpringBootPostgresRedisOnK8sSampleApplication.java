3d3b067a6707016453e826114c85cca9f087b58e
hendisantika <hendisantika@yahoo.co.id>
2026-07-22 16:01:51 +0700

    docs: init project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/jvm/postgresredisonk8s/SpringBootPostgresRedisOnK8sSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.jvm.postgresredisonk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresRedisOnK8sSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgresRedisOnK8sSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/jvm/postgresredisonk8s/SpringBootPostgresRedisOnK8sSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.jvm.postgresredisonk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresRedisOnK8sSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
